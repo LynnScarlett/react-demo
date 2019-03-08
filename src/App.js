237523618ec3fedfe871248129bf4def11d3ceed
朱耀华-电子政务部-北京 <zhuyaohua01@gridsum.com>
2019-03-08 14:03:05 +0800

    todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      list: [],
+      tempName:''
+    }
+    
+  }
+  handleButtonClick=()=> {
+    console.log('button click')
+  }
+
+  addItem = () => {
+    if (this.state.tempName.trim().length) {
+      this.state.list.push({
+        name: this.state.tempName,
+        id:Math.floor(Math.random()*1000)
+      })
+      this.setState({
+        list: this.state.list,
+        tempName:''
+      })
+    }
+
+  }
+
+  handleInputChange=(e)=> {
+    this.setState({
+      tempName:e.target.value
+    })
+  }
+
+  delItem = (index) => {
+    // console.log('del;')
+    this.state.list.splice(index,1)
+    this.setState({
+      list:this.state.list
+    })
+  }
+
   render() {
+    const listItems = this.state.list.map((item,index) =>
+      <div key={item.id} onClick={this.delItem.bind(this,index)}>{item.name}</div>
+    )
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          {listItems}
+        </div>
+        <div>
+          <input value={this.state.tempName} onChange={this.handleInputChange}></input>
+          <button onClick={this.addItem}>add</button>
+        </div>
       </div>
     );
   }
